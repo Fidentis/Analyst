c61f5a7c211137b253ca0541c3e61f535e5ed732
xferkova <ex3me@centrum.sk>
2017-01-17 17:49:15 +0100

    added test folder to gitignore file + files foe kdtree implementation

---
+++ Comparison/src/cz/fidentis/comparison/kdTree/KdTree.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fidentis.comparison.kdTree;
+
+import cz.fidentis.comparison.icp.KdNode;
+import javax.vecmath.Vector3f;
+
+/**
+ *
+ * @author xferkova
+ */
+public interface KdTree {
+
+    /**
+     * Finds nearest neighbor of Point p in KdTree given by root.
+     *
+     * @param p - point of which we want to find nearest neighbor in the tree.
+     * @return  - point representing nearest neighbor of point p
+     */
+    Vector3f nearestNeighbour(Vector3f p);
+    
+    /**
+     * Computes distance to nearest neighbor. If useRelative is true, it will 
+     * compute signed distance.
+     * 
+     * @param p - point to compute the distance to nearest neighbor from
+     * @param pNormal - normal of the point p
+     * @param useRelative - whether to use signed distance or not
+     * @return distance from point p to its nearest neighbor in tree. If useRelative is true,
+     * then signed distance will be computed
+     */
+    double nearestDistance(Vector3f p, Vector3f pNormal, boolean useRelative);
+    
+}
